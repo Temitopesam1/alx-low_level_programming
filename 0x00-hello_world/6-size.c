5129594a0e90111e9170dad99cbb61b9e63527f0
Temitopesam1 <temitopesam5@gmail.com>
2022-06-01 16:30:55 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-#include <conio.h>
+
 
 int main(void) {
 	
-	clrscr();
 	printf("Size of a char: %2d byte(s)\n", sizeof(char));
 	printf("Size of an int: %2d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long));
 	printf("Size of a float: %2d byte(s)\n", sizeof(float));
-	getch();
 	return 0;
 }
