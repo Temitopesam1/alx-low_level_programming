e3a99ea3489a7b091c3cf8b4f63977b417395ad5
Temitopesam1 <temitopesam5@gmail.com>
2022-06-07 12:39:37 -0700

    advance

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,6 +10,6 @@
 int add(int a, int b)
 {
 	int c = a + b;
-	
+
 	return (c);
 }
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int a, b, c, d;
+
+	b = 1;
+	c = 2;
+
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%1d", b);
+		}
+		else
+		{
+			printf('%1d\n", b);
+		}
+		d = b + c;
+		b = c;
+		c = d;
+	}
+	return (0);
+}
