43ba7eb707021a960096be3a8084e52ed420e077
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-07 13:09:59 -0700

    corrected

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,4 +1,4 @@
-#include <stdio.h
+#include <stdio.h>
 
 /**
  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
@@ -16,7 +16,7 @@ int main(void)
 	{
 		if (b != 20365011074)
 		{
-			printf("%ld", b);
+			printf("%ld, ", b);
 		}
 		else
 		{
