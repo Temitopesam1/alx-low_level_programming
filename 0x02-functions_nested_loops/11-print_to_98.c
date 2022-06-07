f9f457062013c6b31eab32a368822fca860e1cfa
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-07 10:41:57 -0700

    corrected

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,20 +9,18 @@
 
 void print_to_98(int n)
 {
-	while (n >= 98)
+	while (n; n >= 99; n--)
 	{
 		if (n != 98)
 		{
 			printf("%d, ", n);
 		}
-		n--;
 	}
-	while (n <= 98)
+	for (n; n < 99; n++)
 	{
 		if (n != 98)
 		{
 			printf("%d, ", n);
 		}
-		n++;
 	}
 }
