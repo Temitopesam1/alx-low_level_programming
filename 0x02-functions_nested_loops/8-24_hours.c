b0bd17c7876081d89a3c4c1e66d5868855e2061b
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-07 09:45:17 -0700

    corrected

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,28 +10,27 @@
 void jack_bauer(void)
 {
 	int a, b, c, d;
-
 	
-        for (a = 48; a <= 50; a++)
-        {
-                for (b = 48; b <= 57; b++)
-                {
-                        if (a == 50 && b == 52)
-                        {
-                                break;
-                        }
-                        for (c = 48; c <= 53; c++)
-                        {
-                                for (d = 48; d <= 57; d++)
-                                {
-                                        _putchar(a);
-                                        _putchar(b);
-                                        _putchar(':');
-                                        _putchar(c);
-                                        _putchar(d);
-                                        _putchar('\n');
-                                }
-                        }
-                }
-        }
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			if (a == 50 && b == 52)
+			{
+				break;
+			}
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
