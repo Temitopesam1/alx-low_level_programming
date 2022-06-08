41a955bbc560d6aae186e8c245a958a3349a00b8
Temitopesam1 <temitopesam5@gmail.com>
2022-06-08 04:27:41 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
+	
 	i = 0;
 
 	/**
@@ -19,8 +19,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
