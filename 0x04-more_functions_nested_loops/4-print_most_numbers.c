e7a66af0b955ff4df8bffa9617ce1a8bcd1fcbfa
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-09 05:43:46 -0700

    correction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,11 +13,11 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i == 2 && i == 4)
+		if (i == 50 && i == 52)
 		{
 			continue;
 		}
 		_putchar(i);
-		_putchar(\n);
+		_putchar('\n');
 	}
 }
