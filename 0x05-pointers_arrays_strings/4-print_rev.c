3d9472c2de2e840a914fa9bb2d03a525945d13f6
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-13 07:37:08 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 /**
  * print_rev -  a function that prints a string, in reverse,
@@ -14,18 +13,17 @@ void print_rev(char *s)
 	char rev[1000];
     	int i, j, count = 0;
 	
-	//finding the length of the string
     	while (s[count] != '\0')
     	{
         	count++;
     	}
     	j = count - 1;
 
-  	//reversing the string by swapping
     	for (i = 0; i < count; i++)
     	{
         	rev[i] = s[j];
         	j--;
     	}
-	puts(rev);
+	_putchar(rev);
+	_putchar('\');
 }
