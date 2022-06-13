116636375d32bb8b29fd34bc3b983a0781228330
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-13 07:10:05 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,6 +11,19 @@
 
 void print_rev(char *s)
 {
-	char c[] = strrev(s);
-	puts(c);
+	 // declare static variable  
+    	static int i, len, temp;
+    
+	len = strlen(s); // use strlen() to get the length of str string
+
+    	if (i < len/2)
+	{  
+        	// temp variable use to temporary hold the string  
+        	temp = s[i];  
+       	 	s[i] = s[len - i - 1];  
+        	s[len - i - 1] = temp;  
+        	i++;
+		puts(s);
+		revstr(s); // recusively calls the revstr() function  
+    	}
 }
