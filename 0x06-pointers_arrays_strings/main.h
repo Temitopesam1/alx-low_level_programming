fd1fd5acee0dc1033b2ae1564410f10864570918
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-15 10:19:37 -0700

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef main.h
-#define main.h
+
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -13,4 +12,3 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-#endif
