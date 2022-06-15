c86f3f954abfb063beb6ff094bc45f40fa608065
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-15 12:22:40 -0700

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,18 +12,11 @@ char *string_toupper(char *str)
 {
 	int i;
 
-<<<<<<< HEAD
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
 	}
-=======
-   	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
-   	}
->>>>>>> 567c778ab44697a7160d3032cf16a16d41440820
+
 	return (str);
 }
