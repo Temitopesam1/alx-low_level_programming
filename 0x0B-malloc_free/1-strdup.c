c151e80ac7a6044732fb6dcc4c499415b18046db
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-28 18:04:30 -0700

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,7 +18,7 @@ char *_strdup(char *str)
 		return (NULL);
 
 	a = strlen(str);
-	b = malloc(sizeof(char) * a);
+	b = malloc(sizeof(char) * a + 1);
 	if (b == NULL)
 		return (NULL);
 	b = strdup(str);
