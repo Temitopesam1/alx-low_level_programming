fe99f9b34d0bc2c576f8913cbf7f302e7e10083f
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-30 10:57:03 -0700

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,32 +10,40 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr1, *ptr2;
-	unsigned int a;
-	
-	ptr1 = ptr;
-	if (new_size == 0 && ptr != NULL)
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
+
+	if (!ptr)
 		return (malloc(new_size));
-	ptr2 = malloc(new_size);
-	if (ptr2 == NULL)
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
 		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
 	if (new_size > old_size)
 	{
-		for (a = 0; a <= old_size; a++)
-			ptr2[a] = ptr1[a];
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
-	if (old_size > new_size)
-	{
-		for (a = 0; a <= new_size; a++)
-			ptr2[a] = ptr1[a];
-	}
-	free (ptr);
-	return (ptr2);
+
+	free(ptr);
+	return (ptr1);
 }
