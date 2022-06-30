09c06c993dfdd6c940acedb25cd87b8e43090f01
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-06-30 10:51:40 -0700

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,40 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int a, b, c, d = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	a = strlen(s1);
-	b = strlen(s2);
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (b <= n)
-		ptr = malloc((a + b + 1) * sizeof(char));
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		ptr = malloc((a + n + 1) * sizeof(char));
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (ptr == NULL)
+	if (!s)
 		return (NULL);
 
-	if (b <= n)
+	while (i < len1)
 	{
-		for (c = 0; c <= a; c++)
-			ptr[c] = s1[c];
-		for (c = a; c <= (a + b); c++)
-		{
-			ptr[c] = s2[d];
-			d++;
-		}
+		s[i] = s1[i];
+		i++;
 	}
-	else
-	{
-		for (c = 0; c <= a; c++)
-			ptr[c] = s1[c];
-		for (c = a; c < (a + n); c++)
-		{
-			ptr[c] = s2[d];
-			d++;
-		}
-	}
-	ptr[c] = '\0';
-	return (ptr);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
