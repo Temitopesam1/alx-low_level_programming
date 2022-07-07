249acb525b5b7a3e5acd4e4414d7afe97dddd996
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-07-07 12:13:33 -0700

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,21 +7,23 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int a;
-	va_list ap;
+	unsigned int i;
+	va_list list;
 
-	va_start(ap, n);
+	va_start(list, n);
 
-	if (separator)
+	for (i = 0; i < n; i++)
 	{
-		for (a = 0; a < n ; a++)
-		{
-			if (a == 0)
-				printf("%d", va_arg(ap, int));
-			else
-				printf("%s%d", separator, va_arg(ap, int));
-		}
+		/*check if separator is not null*/
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d",  separator, va_arg(list, int));
 	}
-	va_end(ap);
+
+	va_end(list);
+
 	printf("\n");
 }
