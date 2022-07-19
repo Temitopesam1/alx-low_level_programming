a2088c2ce475cb02b0ebea0aa2902157a2aaab3b
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-07-19 02:37:26 -0700

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,27 @@
 #include "lists.h"
 
 /**
- * pop_listint - a function that deletes the head node of a listint_t
- * linked list.
- * @head: parameter
- * Return: the head node's data (n).
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ *
+ * Return: head node's data.
  */
 
-int pop_listint(listint_t **head);
+int pop_listint(listint_t **head)
 {
-	listint_t *point;
-	int data;
+	int hnode;
+	listint_t *h;
+	listint_t *curr;
 
-	if (!head || !*head)
+	if (*head == NULL)
+	{
 		return (0);
-
-	data = (*head)->n;
-	point = (*head)->next;
-	free(*head);
-	*head = point;
-
-	return (data);
+	}
+	curr = *head;
+	hnode = curr->n;
+	h = curr->next;
+	free(curr);
+	*head = h;
+	return (hnode);
 }
