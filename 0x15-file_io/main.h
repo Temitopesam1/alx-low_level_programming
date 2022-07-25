305d612f53fabd45802dd2f6e7d9237c6c8627a4
Temi Sam <87991183+Temitopesam1@users.noreply.github.com>
2022-07-25 13:39:35 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,16 +16,15 @@ int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 /* prototypes for ELF */
-void errors(char *msg);
-void pentry(void);
-void ptype(char *buffer);
-void pos(char *buffer);
-void pversion(char *buffer);
-void pABIversion(char *buffer);
-void pdata(char *buffer);
-void pmagic(char *buffer);
-void pclass(char *buffer);
-void ptitle(char *title);
-int main(int argc, char **argv);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
